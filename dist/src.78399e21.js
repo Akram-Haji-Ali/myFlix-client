f39441b8724611439ea72785199273ea32a041de
Akram-Haji-Ali <112483250+Akram-Haji-Ali@users.noreply.github.com>
2023-04-12 23:49:39 +0200

    internal commit

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -29231,7 +29231,11 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+<<<<<<< Updated upstream
   var ws = new WebSocket(protocol + '://' + hostname + ':' + "52265" + '/');
+=======
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64872" + '/');
+>>>>>>> Stashed changes
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
